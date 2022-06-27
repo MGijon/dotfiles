345852310654c4ee2607ceea8b42e317e8703748
Manuel Gijón <mgijon94@gmail.com>
2022-06-27 10:53:32 +0200

    slight changes and Black

--- scripts/rename.py
+++ scripts/rename.py
@@ -15,17 +15,18 @@ args = parser.parse_args()
 path = args.d
 patt = args.p
 
-counter = 1
-for original_name in os.listdir(path):
-    file_extension = original_name.split(".")[-1]
-    original_file = path + "/" + original_name
-    new_name_file = path + "/" + patt + str(counter) + "." + file_extension
+if __name__ == "__main__":
+    counter = 1
+    for original_name in os.listdir(path):
+        file_extension = original_name.split(".")[-1]
+        original_file = path + "/" + original_name
+        new_name_file = path + "/" + patt + str(counter) + "." + file_extension
 
-    try:
-        os.rename(original_file, new_name_file)
-    except Exception as e:
-        print(e)
+        try:
+            os.rename(original_file, new_name_file)
+        except Exception as e:
+            print(e)
 
-    counter += 1
+        counter += 1
 
-print("The name of ", str(counter - 1), " files has been change successfully!!")
+    print(f"The name of {str(counter - 1)} files has been change successfully!!")
